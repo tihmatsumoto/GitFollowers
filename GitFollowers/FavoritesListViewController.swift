96dc2bc01092b0379315ab19867aa31dae5850f2
Marcia Matsumoto <tiemi.matsumoto@gmail.com>
2024-08-23 10:25:39 +0100

    - Add TabBar; - Add NavigationControllers; - Add logo and icons;

---
+++ GitFollowers/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListViewController.swift
+//  GitFollowers
+//
+//  Created by Tiemi Matsumoto on 22/08/2024.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
--- GitFollowers/SearchViewController.swift
+++ GitFollowers/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GitFollowers
 //
 //  Created by Tiemi Matsumoto on 22/08/2024.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemMint
     }
 
-
 }
-
