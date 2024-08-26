df03f5397cff07e9f957333aad27f094f46d18ea
Marcia Matsumoto <tiemi.matsumoto@gmail.com>
2024-08-26 16:48:44 +0100

    - Added collection flow layout; - Added getFollowers to be displayed; - Small refactor

---
+++ GitFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowerCell.swift
+//  GitFollowers
+//
+//  Created by Tiemi Matsumoto on 26/08/2024.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+}
---
+++ GitFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,8 @@
+//
+//  UIHelper.swift
+//  GitFollowers
+//
+//  Created by Tiemi Matsumoto on 26/08/2024.
+//
+
+import Foundation
