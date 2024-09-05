2681aea0c2dd5800268301edd5d9784bb06b0357
Marcia Matsumoto <tiemi.matsumoto@gmail.com>
2024-09-05 15:21:27 +0100

    - Added persistence manager layer - Added UserDefaults

--- GitFollowers/Controllers/FavoritesListViewController.swift
+++ GitFollowers/Controllers/FavoritesListViewController.swift
@@ -11,7 +11,16 @@ class FavoritesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemPink
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
+        
     }
 }
